874d7b847c6bc80f1a356a0ab5538b2564508375
MaksymKundeev <mkundeev@gmail.com>
2022-05-30 14:52:35 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+        new Slider(
+            slides, // слайды
+            'slider', // id для вставки в html
+            {
+                loop: true,
+                navs: true,
+                pags: true,
+                auto: true,
+                delay: 3,
+            }
+        );
